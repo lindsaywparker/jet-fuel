21daa377f9d5fc5a7a1f5c59e3294be146f7ea3c
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-08-21 10:21:24 -0600

    Add console log

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+console.log('WHATEVER');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static('public'));
