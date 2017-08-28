3248d734f804b3808c5717204693039dc8f9f568
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-08-28 11:09:06 -0600

    Add return to redirect next

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const requireHTTPS = (req, res, next) => {
   if (!req.secure) {
     return res.redirect(`https://${req.headers.host}${req.url}`);
   }
-  next();
+  return next();
 };
 
 app.use(requireHTTPS);
